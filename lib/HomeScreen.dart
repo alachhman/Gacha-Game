ef6de01ccd04c08069231203869442a141c6a3d7
Jason-Peralta <45014521+Jason-Peralta@users.noreply.github.com>
2019-03-08 10:05:16 -0500

    #7
    -Moved HomeScreen over to MainScreen in order to implement a new screen for the bottom navigation bar.
    
    Co-Authored-By: alachhman <alachhman@users.noreply.github.com>

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,61 +1,19 @@
 import 'package:flutter/material.dart';
-import 'SummonScreen.dart';
-import 'PlayScreen.dart';
-import 'TeamScreen.dart';
 
-class HomeScreen extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _HomeState();
-  }
-}
+class HomeScreenWidget extends StatelessWidget {
+  HomeScreenWidget();
 
-class _HomeState extends State<HomeScreen> {
-  int _currentIndex = 0;
-  final List<Widget> _children = [
-    TeamScreenWidget(),
-    PlayScreenWidget(),
-    SummonScreenWidget(),
-  ];
 
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.indigo,
-        title: Text("re:"),
-        actions:[
-          new IconButton(
-            icon: new Image.asset('assets/images/icons/boop.png'),
-            onPressed: () => print("icon pressed boi"),
-          ),
-        ],
-    ),
-      body: _children[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: onTabTapped,
-        currentIndex: _currentIndex,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            title: Text('Team'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.colorize),
-            title: Text('Play'),
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.person_add),
-              title: Text('Summon')
-          )
-        ],
-      ),
+      return Scaffold(
+      body: newsCard()// put fucking widgets here,
     );
   }
 
-  void onTabTapped(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-  }
-}
+  Widget newsCard(){
+    return Card();
+
+
+    }
+  } //stlss widget
+
--- lib/SummonScreen.dart
+++ lib/SummonScreen.dart
@@ -1,66 +1,143 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 
 class SummonScreenWidget extends StatelessWidget {
   SummonScreenWidget();
-
+  final List<List<String>> bannerInfo = [
+    ["https://c-3sux78kvnkay76x24mgskvkjogx2eiax78ykijtx2eius.g00.gamepedia.com/g00/3_c-3kdboay.mgskvkjog.ius_/c-3SUXKVNKAY76x24nzzvyx3ax2fx2fmgskvkjog.iax78ykijt.iusx2fkdboay_mgskvkjog_ktx2fjx2fjkx2fMging-Sex78g.vtmx3fbkx78youtx3d6i07g97gih20j65348643l3604g360j6x26o76i.sgx78qx3dosgmk_\$/\$/\$/\$/\$","Banner1"],
+    ['https://c-3sux78kvnkay76x24mgskvkjogx2eiax78ykijtx2eius.g00.gamepedia.com/g00/3_c-3kdboay.mgskvkjog.ius_/c-3SUXKVNKAY76x24nzzvyx3ax2fx2fmgskvkjog.iax78ykijt.iusx2fkdboay_mgskvkjog_ktx2f4x2f4lx2fMging-Omtgiou.vtmx3fbkx78youtx3d44j985kli454jj2g20i594l2837h8l9gx26o76i.sgx78qx3dosgmk_\$/\$/\$/\$/\$',"Banner2"],
+    ['https://c-3sux78kvnkay76x24mgskvkjogx2eiax78ykijtx2eius.g00.gamepedia.com/g00/3_c-3kdboay.mgskvkjog.ius_/c-3SUXKVNKAY76x24nzzvyx3ax2fx2fmgskvkjog.iax78ykijt.iusx2fkdboay_mgskvkjog_ktx2f4x2f4lx2fMging-Omtgiou.vtmx3fbkx78youtx3d44j985kli454jj2g20i594l2837h8l9gx26o76i.sgx78qx3dosgmk_\$/\$/\$/\$/\$',"Banner3"],
+    ['https://c-3sux78kvnkay76x24mgskvkjogx2eiax78ykijtx2eius.g00.gamepedia.com/g00/3_c-3kdboay.mgskvkjog.ius_/c-3SUXKVNKAY76x24nzzvyx3ax2fx2fmgskvkjog.iax78ykijt.iusx2fkdboay_mgskvkjog_ktx2f4x2f4lx2fMging-Omtgiou.vtmx3fbkx78youtx3d44j985kli454jj2g20i594l2837h8l9gx26o76i.sgx78qx3dosgmk_\$/\$/\$/\$/\$',"Banner4"],
+    ['https://c-3sux78kvnkay76x24mgskvkjogx2eiax78ykijtx2eius.g00.gamepedia.com/g00/3_c-3kdboay.mgskvkjog.ius_/c-3SUXKVNKAY76x24nzzvyx3ax2fx2fmgskvkjog.iax78ykijt.iusx2fkdboay_mgskvkjog_ktx2f4x2f4lx2fMging-Omtgiou.vtmx3fbkx78youtx3d44j985kli454jj2g20i594l2837h8l9gx26o76i.sgx78qx3dosgmk_\$/\$/\$/\$/\$',"Banner5"],
+    ['https://c-3sux78kvnkay76x24mgskvkjogx2eiax78ykijtx2eius.g00.gamepedia.com/g00/3_c-3kdboay.mgskvkjog.ius_/c-3SUXKVNKAY76x24nzzvyx3ax2fx2fmgskvkjog.iax78ykijt.iusx2fkdboay_mgskvkjog_ktx2f4x2f4lx2fMging-Omtgiou.vtmx3fbkx78youtx3d44j985kli454jj2g20i594l2837h8l9gx26o76i.sgx78qx3dosgmk_\$/\$/\$/\$/\$',"Banner6"]
+  ];
+  final List<String> pool = ["assets/cherche.png","assets/chrom.png"];
   Widget build(BuildContext context) {
-    return new Scaffold(
-        body: new Container(
-          child: new Column(
+    return Scaffold(
+      body: bannerList(),
+    );
+  }
+  Widget bannerList(){
+    return ListView.builder(
+      scrollDirection: Axis.vertical,
+      itemCount: bannerInfo.length,
+      itemBuilder:(context, position){
+        return banner(context, position);
+      },
+    );
+  }
+  Card banner(BuildContext context, int position){
+    double maxWidth = MediaQuery.of(context).size.width;
+    return Card(
+      margin: new EdgeInsets.all(5.0),
+      child: Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
             children: <Widget>[
-              new Card(
-                child: new Column(
-                  children: <Widget>[
-                    new Padding(
-                      padding: new EdgeInsets.fromLTRB(280, 10, 0, 0),
-                      child: new Row(
-                        children: <Widget>[
-                          new RaisedButton(
-                            child: const Text('Summon Info'),
-                            onPressed: (){
-                              showDialog(
-                                context:context,
-                                child: new Card(
-                                  child: const Text("hello"),
-                                ),
-                              );
-
-                            },
-
-                          ),
-                        ],
-                      ),
-
-
-                    ),
-                    new Image.network('https://educationusa.state.gov/sites/default/files/styles/content_area_full_width/public/field_hei_logo/nyit-logo-pms.jpg?itok=6yOYbJdd'),
-                    new Padding(
-                        padding: new EdgeInsets.fromLTRB(140, 0, 0, 35),
-                        child: new Row(
-                          children: <Widget>[
-                            new RaisedButton(
-                              child: const Text('Summon'),
-                              color: Theme.of(context).accentColor,
-                              elevation: 4.0,
-                              splashColor: Colors.blueGrey,
-                              onPressed: () {
-                                print ("summon pressed");
-                              },
-                            ),
-
-                          ],
-                        )
-                    )
-                  ],
+              InkWell(
+                child: new FlatButton(
+                  color: Colors.blue,
+                  child: Text("More Info"),
+                  splashColor: Colors.blue,
                 ),
+                onTap: (){
+                  print("More Info " + position.toString() + " Tapped");
+                  showDialog(
+                      context: context,
+                      child: Dialog(context, position)
+                  );
+                },
               )
             ],
           ),
-
-        )
-
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ConstrainedBox(
+                  constraints: BoxConstraints(maxWidth: maxWidth * 0.9),
+                  child: Image.network(bannerInfo[position][0],
+                    fit: BoxFit.fill,
+                  )
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                child: Text("Summon"),
+                color: Theme.of(context).accentColor,
+                onPressed: (){
+                  print("Summon " + position.toString() +  " Pressed");
+                  showDialog(
+                    context: context,
+                    child: gacharesults(),
+                  );
+                },
+              )
+            ],
+          )
+        ],
+      ),
+      elevation: 5,
+      color: Colors.white,
     );
-
+  }
+  Card Dialog(BuildContext context, int position){
+    return Card(
+        elevation: 20,
+        margin: EdgeInsets.all(20),
+        child: Column(
+          children: <Widget>[
+            Divider(
+              color: Colors.black,
+              height: 10,
+            ),
+            Text(bannerInfo[position][1]),
+          ],
+        )
+    );
+  }
+  Card gacharesults(){
+    Random rand = new Random();
+    //unit rarity is randomized for now, will be finalized and dependant on the unit in the future.
+    int starCount = rand.nextInt(5);
+    int result = rand.nextInt(pool.length);
+    String obtained = pool[result];
+    return Card(
+      elevation: 20,
+      margin: EdgeInsets.fromLTRB(70, 220, 70, 220),
+      child:
+      Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("You've Obtained:")
+            ],
+          ),
+          Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.asset(obtained)
+              ]
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(Icons.star),
+              Icon(Icons.star),
+              Icon(Icons.star),
+              Icon(Icons.star),
+              Icon(Icons.star),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
