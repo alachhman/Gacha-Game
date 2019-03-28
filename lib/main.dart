5721792ee42cc183509ffcec1c644ced4f0588bd
alachhman.github.io <alachhman@users.noreply.github.com>
2019-03-28 16:19:39 -0400

    #35
    -Made team screen stateful

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:jfinalproject/TeamScreen.dart';
 import 'MainScreen.dart';
 
 
 void main() => runApp(MyApp());
-
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
