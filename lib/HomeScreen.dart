8b71f41dfa3d86fa3fc589b5cad2bdb9acd5678e
WaleedAhmad97 <46979669+WaleedAhmad97@users.noreply.github.com>
2019-03-29 20:48:57 -0400

    #1
    Incorporated 3 iconbuttons. 1 launches webview, the other launches a card

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,36 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
 class HomeScreenWidget extends StatelessWidget {
+
 
   HomeScreenWidget();
 
-
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Foundation()// put fucking widgets here,
+    return new MaterialApp(
+      home: new Foundation(),
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/widget': (_) =>
+        new WebviewScaffold(
+          url: "https://www.google.com/",
+          withZoom: false,
+          withLocalStorage: true,
+        ),
+      },
     );
   }
-  Widget Foundation(){
+}
+
+class Foundation extends StatefulWidget {
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+
+class _MyAppState extends State<Foundation> {
+
+  void _opennewpage() {
+    Navigator.of(context).pushNamed('/widget');
+  }
+
+
+
+
+  Widget build(BuildContext context) {
     return Container(
       color: Color(0xFF4B3F72),
       height: 650,
       child: new Stack(
-        //alignment:new Alignment(x, y)
         children: <Widget>[
           new Positioned(
             child: new Align(
               alignment: FractionalOffset.topLeft,
               child: IconButton(
-                  icon: Icon(Icons.mail),
-                  onPressed: null
+                icon: Icon(Icons.home),
+                onPressed: () {
+                  _opennewpage();
+                },
               ),
             ),
           ),
           new Positioned(
             child: new Align(
-                alignment: FractionalOffset.topCenter,
+              alignment: FractionalOffset.topCenter,
               child: IconButton(
-                  icon: Icon(Icons.cake),
-                  onPressed: null,
+                icon: Icon(Icons.cake),
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                    builder: (BuildContext context) => new Card(
+                      child: const Text("yellow"),
+                    )
+                  );
+                },
               ),
             ),
           ),
@@ -39,12 +76,12 @@ class HomeScreenWidget extends StatelessWidget {
                 alignment: FractionalOffset.topRight,
                 child: IconButton(
                     icon: Icon(Icons.settings),
-                    onPressed: null
+                    onPressed: null,
                 ),
               ))
         ],
       ),
     );
   }
-} //stlss widget
+}
 
