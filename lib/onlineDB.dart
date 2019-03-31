68d576f211c1b501f976faddc87c577669a9e461
Jason-Peralta <45014521+Jason-Peralta@users.noreply.github.com>
2019-03-31 17:52:42 -0400

    #42
    - trying to fix issues

--- lib/onlineDB.dart
+++ lib/onlineDB.dart
@@ -1,98 +1,117 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 
-//Class for userEnergy
-class getUserLevel extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: Firestore.instance.collection('users').snapshots(),
-      builder: (BuildContext context,
-          AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (!snapshot.hasData) return Text('Loading data');
-        return Text(snapshot.data.documents[2]['Level'].toString(),
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
-          color: Colors.black,
-          fontSize: 18,
-        ),);
-      },
-    );
-  }
-}
-
-class getUserEmeralis extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: Firestore.instance.collection('users').snapshots(),
-      builder: (BuildContext context,
-          AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (!snapshot.hasData) return Text('Loading data');
-        return Text(snapshot.data.documents[2]['Emeralis'].toString(),
-          style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.black
-          ),);
-      },
-    );
-  }
-}
-
-//Class to get userName
 class getUserName extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: Firestore.instance.collection('users').snapshots(),
-      builder: (BuildContext context,
-          AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (!snapshot.hasData) return Text('Loading data');
-        return Text(snapshot.data.documents[2]['Name'].toString(),
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
-          color: Colors.black,
-          fontSize: 16,
-        ),);
-      },
+    return FutureBuilder(
+        future: FirebaseAuth.instance.currentUser(),
+        builder: (context, AsyncSnapshot<FirebaseUser> snapshot) {
+          return StreamBuilder(
+              stream: Firestore.instance.collection('users1').document(snapshot.data.uid).snapshots(),
+              builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+                return Text(snapshot.data['displayName'].toString(),
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black
+                  ),);
+              }
+          );
+        }
     );
   }
 }
+
+
+//Class for User Level
+class getUserLevel extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: FirebaseAuth.instance.currentUser(),
+        builder: (context, AsyncSnapshot<FirebaseUser> snapshot) {
+          return StreamBuilder(
+              stream: Firestore.instance.collection('users1').document(snapshot.data.uid).snapshots(),
+              builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+                return Text(snapshot.data['level'].toString(),
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black
+                  ),);
+              }
+          );
+        }
+    );
+  }
+}
+
+
+
+//Class to get userName
+class getUserEmeralis extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: FirebaseAuth.instance.currentUser(),
+        builder: (context, AsyncSnapshot<FirebaseUser> snapshot) {
+          return StreamBuilder(
+              stream: Firestore.instance.collection('users1').document(snapshot.data.uid).snapshots(),
+              builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+                return Text(snapshot.data['emeralis'].toString(),
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black
+                  ),);
+              }
+          );
+        }
+    );
+  }
+}
+
 
 class getUserGold extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: Firestore.instance.collection('users').snapshots(),
-      builder: (BuildContext context,
-          AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (!snapshot.hasData) return Text('Loading data');
-        return Text(snapshot.data.documents[2]['Gold'].toString(),
-          style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.black
-          ),);
-      },
+    return FutureBuilder(
+        future: FirebaseAuth.instance.currentUser(),
+        builder: (context, AsyncSnapshot<FirebaseUser> snapshot) {
+          return StreamBuilder(
+              stream: Firestore.instance.collection('users1').document(snapshot.data.uid).snapshots(),
+              builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+                return Text(snapshot.data['gold'].toString(),
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black
+                  ),);
+              }
+          );
+        }
     );
   }
 }
 
 
-//Doesnt work yet
-// class to the user's Units
-// Firestore.instance.collection('todo_list').where('status', isEqualTo: false).snapshots,
-class GetUserUnits extends StatelessWidget {
+class getUserUnits extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: Firestore.instance.collection('users').snapshots(),
-      builder: (BuildContext context,
-          AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (!snapshot.hasData) return Text('Loading data');
-        return Text(snapshot.data.documents[1]['userUnits'].toString());
-      },
+    return FutureBuilder(
+        future: FirebaseAuth.instance.currentUser(),
+        builder: (context, AsyncSnapshot<FirebaseUser> snapshot) {
+          return StreamBuilder(
+              stream: Firestore.instance.collection('users1').document(snapshot.data.uid).snapshots(),
+              builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+                return Text(snapshot.data['Unit List'].toString(),
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black
+                  ),);
+              }
+          );
+        }
     );
   }
-
 }
