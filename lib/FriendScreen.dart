3d9fe4ca10f9597222ff9fbd7a634f315d925176
WaleedAhmad97 <46979669+WaleedAhmad97@users.noreply.github.com>
2019-04-10 20:26:15 -0400

    #4
    The friends list in no longer hard coded. They are called from 2 2D arrays

--- lib/FriendScreen.dart
+++ lib/FriendScreen.dart
@@ -1,56 +1,62 @@
-
-
 import 'package:flutter/material.dart';
 
 class FriendScreen extends StatelessWidget {
 
   FriendScreen();
-  @override
+
+
+  final List<List<String>> cardInfo = [
+    ["assets/cherche.png", "UserName1", "Level 1"],
+    ["assets/chrom.png", "UserName2" ,"Level 1"],
+    ["assets/ayaka.png", "UserName3", "Level 1"],
+    ["assets/cherche.png", "Username4","Level 1"],
+    ["assets/chrom.png", "Username5","Level 1"],
+    ["assets/ayaka.png", "Username6","Level 1"],
+    ["assets/cherche.png", "Username7","Level 1"],
+  ];
+
+  final List<List<String>> userLVL = [
+    ["Level 1", ""],
+    ["Level 12", ""],
+    ["Level 123", ""],
+    ["Level 1234", ""],
+    ["Level 12345", ""],
+    ["Level 123456", ""],
+    ["Level 1234567", ""],
+  ];
+
   Widget build(BuildContext context) {
-    var card = new Card(
+    return Scaffold(
+      body: bannerList(),
+    );
+  }
+
+  Widget bannerList() {
+    return Container(
+        color: Color(0xFF4B3F72),
+        child: ListView.builder(
+          scrollDirection: Axis.vertical,
+          itemCount: cardInfo.length,
+          itemBuilder: (context, position) {
+            return friendcard(context, position);
+          },
+        )
+    );
+  }
+
+  Card friendcard(BuildContext context, int position) {
+    return Card(
       child: new Column(
         children: <Widget>[
           new ListTile(
-            leading: new Image.asset("assets/ayaka.png"),
-            title: new Text("PlayerName"
-              ,style: new TextStyle(fontWeight: FontWeight.w400),),
-            subtitle: new Text("Player Level"),
+            leading: new Image.asset(cardInfo[position][0]),
+            title: new Text(cardInfo[position] [1]),
+            subtitle: new Text(userLVL[position][0]),
           ),
-          new Divider(color: Colors.black),
-          new ListTile(
-            leading: new Image.asset("assets/chrom.png"),
-            title: new Text("PlayerName2"
-              ,style: new TextStyle(fontWeight: FontWeight.w400),),
-            subtitle: new Text("Player2 Level"),
-          ),
-          new Divider(color: Colors.black),
-          new ListTile(
-            leading: new Image.asset("assets/cherche.png"),
-            title: new Text("PlayerName3"
-              ,style: new TextStyle(fontWeight: FontWeight.w400),),
-            subtitle: new Text("Player3 Level"),
-          ),
-          new Divider(color: Colors.black),
-          new ListTile(
-            leading: new Image.asset("assets/ayaka.png"),
-            title: new Text("PlayerName4"
-              ,style: new TextStyle(fontWeight: FontWeight.w400),),
-            subtitle: new Text("Player4 Level"),
-          ),
-          new Divider(color: Colors.black),
-          new RaisedButton(
-            child: const Text('Add Friends'),
-            color: Colors.black,
 
-          ),
         ],
       ),
     );
-
-    return new Scaffold(
-
-      body: card,
-    );
-
   }
+
 }
