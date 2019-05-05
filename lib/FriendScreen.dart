7b50d7e7e06e945a855dece4b62ff5fea82a6cda
WaleedAhmad97 <46979669+WaleedAhmad97@users.noreply.github.com>
2019-05-05 15:22:54 -0400

    #5
    add friend FAB has been added

--- lib/FriendScreen.dart
+++ lib/FriendScreen.dart
@@ -6,16 +6,58 @@ class FriendScreen extends StatelessWidget {
 
 
   final List<List<String>> cardInfo = [
-    ["assets/cherche.png", "UserName1", "Level 1"],
-    ["assets/chrom.png", "UserName2" ,"Level 1"],
-    ["assets/ayaka.png", "UserName3", "Level 1"],
-    ["assets/cherche.png", "Username4","Level 1"],
-    ["assets/chrom.png", "Username5","Level 1"],
-    ["assets/ayaka.png", "Username6","Level 1"],
-    ["assets/cherche.png", "Username7","Level 1"],
+    ["assets/cherche.png", "UserName1"],
+    ["assets/chrom.png", "UserName2"],
+    ["assets/ayaka.png", "UserName3"],
+    ["assets/cherche.png", "Username4"],
+    ["assets/chrom.png", "Username5"],
+    ["assets/ayaka.png", "Username6"],
+    ["assets/cherche.png", "Username7"],
+    ["assets/cherche.png", "UserName1"],
+    ["assets/chrom.png", "UserName2"],
+    ["assets/ayaka.png", "UserName3"],
+    ["assets/cherche.png", "Username4"],
+    ["assets/chrom.png", "Username5"],
+    ["assets/ayaka.png", "Username6"],
+    ["assets/cherche.png", "Username7"],
+    ["assets/cherche.png", "UserName1"],
+    ["assets/chrom.png", "UserName2"],
+    ["assets/ayaka.png", "UserName3"],
+    ["assets/cherche.png", "Username4"],
+    ["assets/chrom.png", "Username5"],
+    ["assets/ayaka.png", "Username6"],
+    ["assets/cherche.png", "Username7"],
+    ["assets/cherche.png", "UserName1"],
+    ["assets/chrom.png", "UserName2"],
+    ["assets/ayaka.png", "UserName3"],
+    ["assets/cherche.png", "Username4"],
+    ["assets/chrom.png", "Username5"],
+    ["assets/ayaka.png", "Username6"],
+    ["assets/cherche.png", "Username7"],
   ];
 
   final List<List<String>> userLVL = [
+    ["Level 1", ""],
+    ["Level 12", ""],
+    ["Level 123", ""],
+    ["Level 1234", ""],
+    ["Level 12345", ""],
+    ["Level 123456", ""],
+    ["Level 1234567", ""],
+    ["Level 1", ""],
+    ["Level 12", ""],
+    ["Level 123", ""],
+    ["Level 1234", ""],
+    ["Level 12345", ""],
+    ["Level 123456", ""],
+    ["Level 1234567", ""],
+    ["Level 1", ""],
+    ["Level 12", ""],
+    ["Level 123", ""],
+    ["Level 1234", ""],
+    ["Level 12345", ""],
+    ["Level 123456", ""],
+    ["Level 1234567", ""],
     ["Level 1", ""],
     ["Level 12", ""],
     ["Level 123", ""],
@@ -27,11 +69,13 @@ class FriendScreen extends StatelessWidget {
 
   Widget build(BuildContext context) {
     return Scaffold(
-      body: bannerList(),
+      body: friendList(),
+      floatingActionButton: friendButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
     );
   }
 
-  Widget bannerList() {
+  Widget friendList() {
     return Container(
         color: Color(0xFF4B3F72),
         child: ListView.builder(
@@ -44,6 +88,24 @@ class FriendScreen extends StatelessWidget {
     );
   }
 
+  Widget friendButton(){
+    return new Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        new FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: null,
+            elevation: 20.0,
+          tooltip: "add friend",
+          backgroundColor: Color(0xFF4B3F72),
+
+        )
+      ],
+
+
+    );
+  }
+
   Card friendcard(BuildContext context, int position) {
     return Card(
       child: new Column(
@@ -51,12 +113,41 @@ class FriendScreen extends StatelessWidget {
           new ListTile(
             leading: new Image.asset(cardInfo[position][0]),
             title: new Text(cardInfo[position] [1]),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            onTap: () {
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) => new Card(
+                      margin: EdgeInsets.fromLTRB(20, 120, 20, 350),
+                      child: new Column(
+                        children: <Widget>[
+                          new RaisedButton(
+                            child: new Text("Remove Friend"),
+                              onPressed: null,
+                            color: Color(0xFF4B3F72),
+                          ),
+                          new RaisedButton(
+                              child: new Text("Send Gift"),
+                              onPressed: null,
+                            disabledColor: Color(0xFF4B3F72),
+
+                          ),
+                        ],
+                      ),
+                    )
+                );
+              },            
+            onLongPress: (){
+              print('tapped!!!');
+            },
             subtitle: new Text(userLVL[position][0]),
           ),
 
         ],
+
+
       ),
+
     );
   }
-
 }
