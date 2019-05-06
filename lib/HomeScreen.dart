4cedc4c52bd475e872f2fccec6b390e0ac4f004c
Jason-Peralta <45014521+Jason-Peralta@users.noreply.github.com>
2019-05-05 21:22:45 -0400

    #52
    -final push

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -7,7 +7,7 @@ import 'auth.dart';
 
 class HomeScreenWidget extends StatelessWidget {
   bool vicCon;
-  var victoryString = 'placeholder';
+  var victoryString = 'Victory';
   HomeScreenWidget();
 
   Widget build(BuildContext context) {
@@ -98,7 +98,6 @@ class HomeScreenWidget extends StatelessWidget {
                 ),
               ),
           Align(
-
               alignment: Alignment(0,0),
               child: IconButton(
                   icon: Icon(Icons.save),
@@ -106,19 +105,71 @@ class HomeScreenWidget extends StatelessWidget {
                     showDialog(
                         context: context,
                         builder: (BuildContext context) => new Card(
-                          color: Color(0xFF4B3F72),
+                          color: Color(0xFF1F2041),
                           margin: EdgeInsets.fromLTRB(20, 40, 20, 40),
                           child: new Stack(
                             children: <Widget>[
                               Align(
-                                  alignment: Alignment(0,-.85),
-                                    child: Text(victoryString,
-                                    style: TextStyle(
-                                    fontFamily: 'JosefinSans',
-                                    fontSize: 42,
-                                    fontWeight: FontWeight.w700
-                                  ),
-                                )
+                                alignment: Alignment(0, 0),
+                                child: Container(
+                                  color: Color(0xFF4B3F72),
+                                  height: 655,
+                                  width: 355,
+                                  child: new Stack(
+                                      children: <Widget>[
+                                        Align(
+                                            alignment: Alignment(0,-.85),
+                                            child: Text(victoryString,
+                                              style: TextStyle(
+                                                  fontFamily: 'JosefinSans',
+                                                  fontSize: 42,
+                                                  fontWeight: FontWeight.w700
+                                              ),
+                                            )
+                                        ),
+                                        Align(
+                                          alignment: Alignment(0,0),
+                                        child: Container(
+                                            height: 40,
+                                            width:335,
+                                            child: new InputDecorator(
+                                      decoration: new InputDecoration(
+                                          labelText: 'XP',
+                                          border: OutlineInputBorder(
+                                            borderSide: const BorderSide(color: Colors.white),
+                                          ),
+                                          labelStyle: new TextStyle(color: Colors.yellow)
+                                      )
+                                         )
+                                        )
+                                     ),
+                                        Align(
+                                      alignment: Alignment(0,-.5),
+                                       child: Container(
+                                            height: 40,
+                                            width:335,
+                                           child: new InputDecorator(
+                                            decoration: new InputDecoration(
+                                                labelText: 'Emeralis',
+                                                border: OutlineInputBorder(
+                                                  borderSide: const BorderSide(color: Colors.white),
+                                                ),
+                                                labelStyle: new TextStyle(color: Colors.green)
+                                            )
+                                        )
+                                        )
+                                        ),
+                                        Align(
+                                          alignment: Alignment(0,.85),
+                                            child: RaisedButton(
+                                            child: const Text('Continue'),
+                                         color: Color(0xFF1F2041),
+                                        onPressed: () {}
+                                        )
+                                        )
+                                  ]
+                               )
+                               )
                               )
                             ],
                           )
